f7cd89b0891ab15cb6c69600e888f000d420f662
FihudAmien <Fihudamien72@gmail.com>
2021-11-24 21:22:32 +0700

    first commit

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void exit(){
+        Application.Quit();
+    }
+    public void play(string scene_name){
+        Application.LoadLevel(scene_name);
+    }
+}
